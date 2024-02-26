0c1afe06a509cdc3ce411576a57506e84ab14542
guiijoo <gui.josepette@gmail.com>
2024-02-26 00:44:51 -0300

    muita coisa pqp

---
+++ CindereloBaiano/Assets/Scenes/Menu/MenuController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void NovoJogo()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("Day");
+    }
+
+    public void Comecar()
+    {
+        SceneManager.LoadScene("Day");
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+    }
+}
